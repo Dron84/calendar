7f1548dc56c7f27d39fd37f5a223731e9137b43b
Андрей Шарунов <dron84@gmail.com>
2020-07-13 10:39:55 +0300

    up

--- src/views/calendar.vue
+++ src/views/calendar.vue
@@ -165,8 +165,8 @@ export default {
         return {caption: '火', color: 'red'}
       }else if(stolp.sky ==='庚' && stolp.ground === '子' ||
       stolp.sky ==='辛' && stolp.ground === '丑' ||
-      stolp.sky ==='庚' && stolp.ground === '子' ||
-      stolp.sky ==='辛' && stolp.ground === '丑' ||
+      stolp.sky ==='戊' && stolp.ground === '寅' ||
+      stolp.sky ==='己' && stolp.ground === '卯' ||
       stolp.sky ==='丙' && stolp.ground === '辰' ||
       stolp.sky ==='丁' && stolp.ground === '巳' ||
       stolp.sky ==='庚' && stolp.ground === '午' ||
@@ -176,8 +176,8 @@ export default {
       stolp.sky ==='丙' && stolp.ground === '戌' ||
       stolp.sky ==='丁' && stolp.ground === '亥' ){
         return {caption: '土', color: 'brown'}
-      }else if(stolp.sky ==='丙' && stolp.ground === '子' ||
-      stolp.sky ==='丁' && stolp.ground === '丑' ||
+      }else if(stolp.sky ==='甲' && stolp.ground === '子' ||
+      stolp.sky ==='乙' && stolp.ground === '丑' ||
       stolp.sky ==='壬' && stolp.ground === '寅' ||
       stolp.sky ==='癸' && stolp.ground === '卯' ||
       stolp.sky ==='庚' && stolp.ground === '辰' ||
